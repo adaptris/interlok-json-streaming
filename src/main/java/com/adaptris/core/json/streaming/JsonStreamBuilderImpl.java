0392c6cf3d095415644e35a6385c46aa0ab5d563
Lewin Chan <lewin.chan@adaptris.com>
2020-08-20 16:01:48 +0100

    Lombokify

--- src/main/java/com/adaptris/core/json/streaming/JsonStreamBuilderImpl.java
+++ src/main/java/com/adaptris/core/json/streaming/JsonStreamBuilderImpl.java
@@ -1,22 +1,31 @@
 package com.adaptris.core.json.streaming;
 
+import javax.validation.Valid;
+import org.apache.commons.lang3.ObjectUtils;
+import com.adaptris.annotation.AdvancedConfig;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
 public abstract class JsonStreamBuilderImpl {
+
+  /**
+   * Configuration that dictates the behaviour of the underlying streaming instances.
+   *
+   */
+  @Getter
+  @Setter
+  @Valid
+  @AdvancedConfig
   private JsonStreamingConfigBuilder config;
-
-  public JsonStreamingConfigBuilder getConfig() {
-    return config;
-  }
-
-  public void setConfig(JsonStreamingConfigBuilder builder) {
-    this.config = builder;
-  }
 
   public <T extends JsonStreamBuilderImpl> T withConfig(JsonStreamingConfigBuilder b) {
     setConfig(b);
     return (T) this;
   }
-  
+
   public JsonStreamingConfigBuilder config() {
-    return getConfig() != null ? getConfig() : new JsonStreamingConfigBuilder();
+    return ObjectUtils.defaultIfNull(getConfig(), new JsonStreamingConfigBuilder());
   }
 }
--- src/main/java/com/adaptris/core/json/streaming/JsonStreamingOutputFactory.java
+++ src/main/java/com/adaptris/core/json/streaming/JsonStreamingOutputFactory.java
@@ -1,25 +1,21 @@
 package com.adaptris.core.json.streaming;
 
 import javax.xml.stream.XMLOutputFactory;
-
 import com.adaptris.stax.XmlOutputFactoryBuilder;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
-
 import de.odysseus.staxon.json.JsonXMLOutputFactory;
+import lombok.NoArgsConstructor;
 
 /**
  * {@link com.adaptris.stax.XmlOutputFactoryBuilder} implementation that allows us to write JSON via standard
  * {@code javax.xml.stream} interfaces.
- * 
+ *
  * @config json-streaming-output
  * @since 3.8.3
  */
 @XStreamAlias("json-streaming-output")
+@NoArgsConstructor
 public class JsonStreamingOutputFactory extends JsonStreamBuilderImpl implements XmlOutputFactoryBuilder {
-
-  public JsonStreamingOutputFactory() {
-
-  }
 
   @Override
   public XMLOutputFactory build() {
